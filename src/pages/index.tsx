d09d17b2c017f9f42b9c9580182ac483ba1e325f
Marcelo Junior <designtechti.dev@protonmail.com>
2025-07-26 20:57:30 -0300

    :art: Style(globalStyles): Global Styles :art: Add global styles and update colors in the theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import { styled } from '../styles';
+import { styled } from "../styles";
 
 const Button = styled("button", {
   backgroundColor: "$rocketseat",
   borderRadius: 4,
   border: 0,
   padding: "4px 8px",
-  color: 'white',
+  color: "$white",
 
   span: {
     fontWeight: "bold",
