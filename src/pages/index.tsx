ebdc1721fabbcc18d4477cc115c595fdbbb10352
Marcelo Junior <designtechti.dev@protonmail.com>
2025-07-26 22:48:16 -0300

    :rocket: Feat: Application Structure :rocket: Displays the initial structure of the product view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,39 @@
-import { styled } from "../styles";
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
 
-const Button = styled("button", {
-  backgroundColor: "$rocketseat",
-  borderRadius: 4,
-  border: 0,
-  padding: "4px 8px",
-  color: "$white",
-
-  span: {
-    fontWeight: "bold",
-  },
-
-  "&:hover": {
-    filter: "brightness(0.8)",
-  },
-});
+import camiseta1 from "../assets/camisetas/1.png";
+import camiseta2 from "../assets/camisetas/2.png";
+import camiseta3 from "../assets/camisetas/3.png";
 
 export default function Home() {
   return (
-    <Button>
-      <span>🔥🚀</span>O Foguete vai subir
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,99</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,99</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,99</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
