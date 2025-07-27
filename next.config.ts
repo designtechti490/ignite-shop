a07ebe4e432657acc5e04a97c38b9e54a99d744b
Marcelo Junior <designtechti.dev@protonmail.com>
2025-07-26 21:23:19 -0300

    :rocket: Feat(Header): Application Header :rocket:
    - Updated the Next.js configuration to enable future image support.
    - Integrated a logo image into the app header using Next.js Image component.
    - Created styled components for the app layout, including a container and header.
    - Added a new SVG logo asset to the project.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@
 const nextConfig = {
   reactStrictMode: true,
   // swcMinify: true,
+  experimental: {
+    images: {
+      allowFutureImage: true,
+    },
+  },
 };
 
 module.exports = nextConfig;
