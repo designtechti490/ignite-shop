1833475765d6256928e11ca0d2931f1a2c9e8dca
Marcelo Junior <designtechti.dev@protonmail.com>
2025-08-05 22:02:27 -0300

    :rocket: Feat: Purchase data on success :rocket: Retrieves the purchase data and inserts it into the success page to display the purchase made with the API data

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,12 +1,25 @@
 import Link from "next/link";
 import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+import Image from "next/image";
+import { GetServerSideProps } from "next";
+import Stripe from "stripe";
+import { stripe } from "../lib/stripe";
+interface SuccessProps {
+  costumerName: string;
+  product: {
+    name: string;
+    imageUrl: string;
+  };
+}
 
-export default function Success() {
+export default function Success({ costumerName, product }: SuccessProps) {
   return (
     <SuccessContainer>
       <h1>Compra efetuada</h1>
 
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={120} height={110} alt="" />
+      </ImageContainer>
 
       <p>
         Uhuul <strong>Marcelo Junior</strong>, sua{" "}
@@ -18,3 +31,24 @@ export default function Success() {
     </SuccessContainer>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const sessionId = String(query.session_id);
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ["line_items", "line_items.data.price.product"],
+  });
+
+  const costumerName = session.customer_details.name;
+  const product = session.line_items.data[0].price.product as Stripe.Product;
+
+  return {
+    props: {
+      costumerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0],
+      },
+    },
+  };
+};
